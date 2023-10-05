6b090813d3ff522a8869467e633210d213c91b0c
claudiayip <claudiawingtungyip@gmail.com>
2023-10-06 00:41:31 +0100

    Add example on Simple Moving Average

---
+++ Analysis/time_series_analysis.py
@@ -0,0 +1,18 @@
+from Data.get_market_data import load_stock_price
+from sklearn.metrics import mean_squared_error
+
+airbus_ts = load_stock_price(90, 'AIR.PA')
+
+airbus_close = airbus_ts['4. close']
+
+train = airbus_close.iloc[0:59]
+test = airbus_close.iloc[59:89]
+
+# Simple Moving Average Example
+sma_pred = train.rolling(window=5).mean()
+
+# TODO CY: add MSE between test and sma_pred
+# np.sqrt(mean_squared_error(sma_pred, train)
+
+print(train)
+print(test)
--- Data/get_market_data.py
+++ Data/get_market_data.py
@@ -1,9 +1,11 @@
 from alpha_vantage.timeseries import TimeSeries
-import pandas as pd
-import numpy as np
 
+# API Key
 key = 'HIIPEUKMK60DTO96'
 ts = TimeSeries(key, output_format='pandas')
 
 
-airbus_ts, meta = ts.get_daily('AIR.PA', outputsize='full')
+def load_stock_price(days, ticker, outputsize='full'):
+    stock_ts = ts.get_daily(ticker, outputsize)[0]
+    return stock_ts.iloc[0:days-1][::-1]
+
