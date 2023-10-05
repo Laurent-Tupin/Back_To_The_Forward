d524862fb8c5ec7ea2b44621aa5f87520950b8e1
Laurent Tupin <64402274+Laurent-Tupin@users.noreply.github.com>
2023-10-05 19:52:37 +0800

    Update get_market_data.py

--- Data/get_market_data.py
+++ Data/get_market_data.py
@@ -2,9 +2,9 @@ from alpha_vantage.timeseries import TimeSeries
 import pandas as pd
 import numpy as np
 
+
 key = 'HIIPEUKMK60DTO96'
 ts = TimeSeries(key, output_format='pandas')
-
 
 airbus_ts, meta = ts.get_daily('AIR.PA', outputsize='full')
 print(airbus_ts)
