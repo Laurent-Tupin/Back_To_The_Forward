542e3d29d4624fe6aba8904f021b6b0172182c30
claudiayip <claudiawingtungyip@gmail.com>
2023-10-09 19:36:56 +0100

    Add feature to save market data and then load from local directory

--- Analysis/time_series_analysis.py
+++ Analysis/time_series_analysis.py
@@ -1,13 +1,10 @@
 import numpy as np
 import pandas as pd
-from Data.get_market_data import load_stock_price
 from sklearn.metrics import mean_squared_error
-
-length_train = 360
-stock_price_col = '4. close'
+from Data_processing.get_market_data import load_stock_price
 
 
-def SMA_window_with_lowest_MSE(ticker, stock_price_col, length_train, min_window_length, n):
+def SMA_window_with_lowest_MSE(ticker, stock_price_col, length_train, min_window_length, n, API=True):
     """ Find window length with lowest MSE by looping over n different lengths of windows
 
     Parameters:
@@ -16,6 +13,7 @@ def SMA_window_with_lowest_MSE(ticker, stock_price_col, length_train, min_window
         length_train: length of the training set
         min_window_length: minimum length of window
         n: size of loop
+        API: use API data by default, otherwise loads from local directory
 
 
     Returns:
@@ -23,7 +21,7 @@ def SMA_window_with_lowest_MSE(ticker, stock_price_col, length_train, min_window
         float: MSE
     """
 
-    stock = load_stock_price(length_train, ticker)[stock_price_col]
+    stock = load_stock_price(length_train, ticker, API)[stock_price_col]
     train = stock.iloc[0:(length_train - 1)]
     MSE_list = []
     for i in range(min_window_length, n):
@@ -34,11 +32,7 @@ def SMA_window_with_lowest_MSE(ticker, stock_price_col, length_train, min_window
         MSE_list = MSE_list + [MSE]
 
     # TODO CY or LT: how can we write this in a neater way?
-    MSE_df = pd.DataFrame({'window_length': range(min_window_length, n), 'MSE': MSE_list}, index=None)
+    MSE_df = pd.DataFrame({'window_length': range(min_window_length, n),
+                           'MSE': MSE_list})
     min_MSE = MSE_df['MSE'] == np.min(MSE_df['MSE'])
-
     return int(MSE_df.loc[min_MSE, 'window_length'].iloc[0]), np.min(MSE_df['MSE'])
-
-
-# Airbus example
-airbus_window, airbus_MSE = SMA_window_with_lowest_MSE('AIR.PA', stock_price_col, length_train, 10, 59)
---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+key = 'HIIPEUKMK60DTO96'
+data_dir = os.path.join(os.getcwd(), "data")
+length_train = 360
+stock_price_col = '4. close'
+
