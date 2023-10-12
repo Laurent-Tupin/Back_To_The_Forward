7f06ac917511eb0fbb4c6f23a8d7a025144c0637
claudiayip <claudiawingtungyip@gmail.com>
2023-10-12 02:03:34 +0100

    Add function to identify the dates when a long SMA crosses a short SMA for a stock

--- Analysis/time_series_analysis.py
+++ Analysis/time_series_analysis.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
 from Data_processing.get_market_data import load_stock_price
 
@@ -36,3 +37,26 @@ def SMA_window_with_lowest_MSE(ticker, stock_price_col, length_train, min_window
                            'MSE': MSE_list})
     min_MSE = MSE_df['MSE'] == np.min(MSE_df['MSE'])
     return int(MSE_df.loc[min_MSE, 'window_length'].iloc[0]), np.min(MSE_df['MSE'])
+
+
+def plot_two_SMA(stock: pd.Series, long_mean: int, short_mean: int):
+    plt.figure()
+    plt.plot(stock.rolling(window=long_mean).mean(), label="long_mean")
+    plt.plot(stock.rolling(window=short_mean).mean(), label="short_mean")
+    plt.plot(stock)
+
+
+def identify_series_crossing(stock: pd.Series, long_mean: int, short_mean: int):
+    long_SMA = stock.rolling(window=long_mean).mean().dropna().rename("long_SMA")
+    short_SMA = stock.rolling(window=short_mean).mean().iloc[(long_mean-1):].rename("short_SMA")
+    new_df = pd.concat([long_SMA, short_SMA], axis=1)
+
+    short_higher = new_df.apply(lambda x: (1 if (x['long_SMA'] < x['short_SMA']) else 0), axis=1)
+    short_higher_t = short_higher.iloc[1:].reset_index(drop=True)
+    short_higher_tp1 = short_higher.iloc[:-1].reset_index(drop=True)
+
+    new_df = new_df.iloc[1:]
+    cross = ((short_higher_t + short_higher_tp1) == 1).rename("cross")
+    new_df = pd.concat([new_df, pd.DataFrame(cross).set_index(new_df.index)], axis=1)
+    new_df.loc[new_df['cross']].index.to_list()
+    return new_df.loc[new_df['cross']].index.to_list()
